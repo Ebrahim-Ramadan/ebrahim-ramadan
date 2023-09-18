b0aff7c6fc9c6f9417d6cf62a7a9cba438399203
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-19 01:09:04 +0200

    nextseo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Head from 'next/head';
+import { NextSeo } from 'next-seo';
+
 const inter = Inter({ subsets: ['latin'] })
 
 
@@ -8,6 +10,26 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <NextSeo
+        title="My Page Title"
+        description="Description of my page"
+        openGraph={{
+          title: 'My Page Title',
+          description: 'Description of my page',
+          images: [
+            {
+              url: 'https://images.pexels.com/photos/14130600/pexels-photo-14130600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
+              width: 1200, 
+              height: 630, 
+              alt: 'My Thumbnail',
+            },
+          ],
+        }}
+        twitter={{
+          handle: '@scoopsahoykid', // Optional: Your Twitter handle
+          cardType: 'summary_large_image',
+        }}
+      />
        <head>
         <link rel="icon" href="/favicon.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
