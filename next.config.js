8e84588b862223306b800eda8e6918d4e4f6526d
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-24 21:51:21 +0200

    fixed webpack err

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   env: {
     GITHUB_BAT_ACCESS_TOKEN : "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",
   },
+  
     images: {
       domains: ['http.cat'], 
     },
