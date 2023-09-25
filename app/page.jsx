772ef7615f650e0d455ec171250a327a98c2cbb4
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-25 17:16:20 +0200

    removed mui materials

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,15 @@
 import TerminalSearch from '../components/TerminalSearch'
 import Header from '../components/Header'
 import Head from 'next/head'
-import gb from '/public/github.svg'
 import {NextUIProvider} from "@nextui-org/react";
 export default function Home() {
   return (
     <NextUIProvider>
             <Head>
-        <title>data.title</title>
+        <title>Ebrahim.Ramadan.portfolio</title>
         
-        <meta name="description" content='contentdesc'/>
+        <meta name="description" content='Js. Software developer'/>
 
-        <meta property="og:image" content={gb} />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
       </Head>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,16 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import GitHubIcon from '@mui/icons-material/GitHub';
-import Chip from '@mui/material/Chip';
+import {GhIcon} from "../public/github";
+import gh from "../public/gh.svg";
+import {Chip, Avatar} from "@nextui-org/react";
  const Header = () => {
   
   return (
     <div className='flex justify-end p-1'>
             <a  href='https://github.com/ebrahim-Ramadan' target='_blank' className='p-1'>
-            <Chip icon={<GitHubIcon />} label="Github" variant="filled" color="info"/>
+        <Chip color="default" variant="bordered" startContent={<GhIcon />} style={{color:'#EEE'}}
+        >Github</Chip>
       </a>
 </div>
   )
