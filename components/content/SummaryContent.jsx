ee5153c7432e3dddcca9ff26a4639d450351851c
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-09-29 19:55:51 +0200

    added 'see resume', modified summary content

--- components/content/SummaryContent.jsx
+++ components/content/SummaryContent.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import {External} from "/public/External";
+import { Chip } from "@nextui-org/react";
+import { useRouter } from 'next/navigation';
 const SummaryContent = () => {
+  const router = useRouter()
   return (
     <div className='summaryResponse text-blue-200 text-xs'>
       <br />
-      <p>Name: Ebrahim Ramadan</p>
+      <p>Iam Ebrahim Ramadan</p>
+      <p>part-time Front-End developer</p>
       <br />
-      <p>ramadanebrahim791@gmail.com</p>
+      <p>AND I FREAKING LOVE CATS</p>
       <br />
+      <br />
+      <p>see:</p>
       <br />
       <a href='https://www.linkedin.com/in/ebrahim-ramadan-a9a9991a6/' target='_blank'>
         { '>'} LinkedIn.Ebrahim-Ramadan <span>&nbsp;</span>
@@ -24,8 +30,19 @@ const SummaryContent = () => {
       </a>
       <br />
       <br />
+      <a href='https://twitter.com/scoopsahoykid' target='_blank'>
+        { '>'} twt.Ebrahim-Ramadan <span>&nbsp;</span>
+      </a>
       <br />
       <br />
+      
+      <Chip color="primary" size="sm" radius="lg" variant="flat" endContent={<External />} style={{ color: '#EEE' }}
+        className='transition-all  duration-300 ease-in-out hover:opacity-75 p-2 flex flex-row justify-center items-center h-8 gap-x-1 cursor-pointer'
+        onClick={()=>router.push('https://drive.google.com/file/d/15sF0Clw9svljEaltBY_mRYzh22ExZf5K/view?usp=drive_link')}
+        >
+        my resume
+          </Chip>
+      
     </div>
   );
 };
