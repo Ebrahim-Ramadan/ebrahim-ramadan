f24902de3556d5dbdad84e98de08b90bd603da1c
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-10-31 18:16:37 +0200

    added implemeentation of nextauth.js to the app, managed sessions, but not for the jwt pt

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,18 @@
 'use client'
+
+import { SessionProvider } from "next-auth/react"
 import Head from 'next/head'
 import {NextUIProvider} from "@nextui-org/react";
 import { HeroParticles  } from '@/components/Particles/HeroParticles';
-export default function Home() {
+export default function Home(
+  {
+    Component,
+    pageProps: { session, ...pageProps },
+  }
+) {
   return (
+    <SessionProvider session={session}>
+
     <NextUIProvider>
             <Head>
         <title>Ebrahim.Ramadan.portfolio</title>
@@ -13,8 +22,10 @@ export default function Home() {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
       </Head>
-      <HeroParticles />
+        <HeroParticles {...pageProps}/>
     </NextUIProvider>
+    </SessionProvider>
 
+      
   )
 }
