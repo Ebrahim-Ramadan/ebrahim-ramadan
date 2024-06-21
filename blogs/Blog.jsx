ce308f5ff0e15f5af6f623e28e8c31774ba2e6ff
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-06-21 17:41:11 +0200

    fixed build logs issues

--- blogs/Blog.jsx
+++ blogs/Blog.jsx
@@ -15,17 +15,17 @@ export const Blog = () => {
       </div>
       <div className='flex flex-col gap-4 w-full h-full'>
       {blogsData.map((blog) => (
-        <a href={`blogs/${blog.slug}`} key={blog.id} className=" backdrop-blur-3xl transition duration-200 bg-white/10 hover:bg-white/20 rounded-lg h-full w-full overflow-hidden">
+        <a href={`blogs/${blog.slug}`} key={blog.id} className="border border-2 border-white/10 backdrop-blur-3xl transition duration-200 bg-white/10 hover:bg-white/20 rounded-lg h-full w-full overflow-hidden">
           <Image
             src={blog.bg}
             width={500}
             height={500}
             alt={blog.title}
-            className=" inset-0 w-full h-full -z-10"
+            className="bg-white/20 inset-0 w-full h-full -z-10"
           />
           <div className="  flex flex-col justify-end p-4 z-10">
-            <p className="md:text-3xl text-2xl font-medium">{blog.title}</p>
-            <p className="text-gray-300 text-end text-xs">{blog.date}</p>
+            <p className="md:text-xl text-lg font-bold">{blog.title}</p>
+            <p className="text-gray-300 text-end text-xs mt-4">{blog.date}</p>
           </div>
         </a>
       ))}
