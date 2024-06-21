f51cb83f15360d1ad15407eeb896e937e092236e
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-06-21 19:21:23 +0200

    changed metadata desc

--- app/blogs/GitLFS/page.js
+++ app/blogs/GitLFS/page.js
@@ -8,11 +8,11 @@ export default async function Home() {
 }
 export const metadata = {
     title: "Git LFS",
-    description: "Git LFS",
+    description: "Git Large File Storage",
     openGraph: {
         images: ['https://avatars.githubusercontent.com/u/65041082?s=400&u=cb58112cd92067eb53afe77fc7beb1573aab869d&v=4'],
         title: 'Git LFS',
-        description: 'Git LFS',
+        description: 'Large File Storage',
     },
     title: {
         default: 'Git LFS',
