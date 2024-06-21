77d2244c4ed2cff74a847fd0af71727fb56f7916
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-06-21 18:08:56 +0200

    changes some capitalized ltrs

--- blogs/BlogsData.js
+++ blogs/BlogsData.js
@@ -1,7 +1,7 @@
 
 export const blogsData = [
     {
-        id: 1, title: 'Git LFS (large file system) hell', bg:'/blogs/lfs.jpg' , date:'2024-06-14', slug:'GitLFS'},
-    { id: 1, title: 'Next.js Routes (The FullStack)', bg: '/blogs/nextjs.jpg' , date:'2022-10-01', slug:''},
-    { id: 1, title: 'I am a tailwind guy, best thing I ever did', bg: '/blogs/tailwind.jpg' , date:'2023-10-11', slug:''},
+        id: 1, title: 'Git LFS (large file system) hell', bg:'/Blogs/lfs.jpg' , date:'2024-06-14', slug:'GitLFS'},
+    { id: 1, title: 'Next.js Routes (The FullStack)', bg: '/Blogs/nextjs.jpg' , date:'2022-10-01', slug:''},
+    { id: 1, title: 'I am a tailwind guy, best thing I ever did', bg: '/Blogs/tailwind.jpg' , date:'2023-10-11', slug:''},
 ]
