2f14776c101762ba70e75bf4c9c4fb5ba166f16b
Ebrahim Ramadan <65041082+Ebrahim-Ramadan@users.noreply.github.com>
2025-02-09 00:13:45 +0200

    Delete blogs directory

--- blogs/BlogsData.js
+++
@@ -1,8 +0,0 @@
-
-export const blogsData = [
-    { id: 1, title: 'Git LFS (large file system) hell', bg: '/Blogs/lfs.jpg', date: 'May 12, 2024', slug: 'GitLFS' },
-    { id: 4, title: 'Github API, How Cool It Is', bg: '/Blogs/gihutb-api.png' , date:'June 19, 2023', slug:'GithubApi'},
-    { id: 2, title: 'Next.js Routes (The FullStack)', bg: '/Blogs/nextjs.jpg' , date:'September 15, 2023', slug:'NextJsApi'},
-    { id: 3, title: 'I am a tailwind guy, best thing I ever did', bg: '/Blogs/tailwind.jpg', date: 'July 15, 2023', slug: 'Tailwind' },
-
-]
--- blogs/GithubApi/GithubApi.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const GithubApi = () => {
-  return (
-    <div>GithubApi</div>
-  )
-}
