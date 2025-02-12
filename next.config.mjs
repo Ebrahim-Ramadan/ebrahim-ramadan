c726f8fafeb9f876d95fabe7f0d4676bba879fbc
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-02-12 05:14:04 +0200

    fixes mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
-import createMDX from '@next/mdx'
- 
+import createMDX from '@next/mdx';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+
+  // Configure images to allow 'media2.dev.to' as a hostname
+  images: {
+    domains: ['media2.dev.to'],
+  },
+
   // Optionally, add any other Next.js config below
-}
- 
+};
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-})
- 
+});
+
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig)
+export default withMDX(nextConfig);
