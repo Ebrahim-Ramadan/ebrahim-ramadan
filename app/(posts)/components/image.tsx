380336a314013618966138b53376d7efd5212130
Ebrahim Ramadan <65041082+Ebrahim-Ramadan@users.noreply.github.com>
2026-09-05 21:51:45 +0000

    /view cv

--- app/(posts)/components/image.tsx
+++ app/(posts)/components/image.tsx
@@ -16,78 +16,82 @@ export async function Image({
   height: number | null;
 }) {
   const isDataImage = src.startsWith("data:");
+  const isRemoteImage = src.startsWith("http");
   if (isDataImage) {
     /* eslint-disable @next/next/no-img-element */
     return <img src={src} alt={originalAlt ?? ""} />;
+  }
+
+  let alt: string | null = null;
+  let dividedBy = 100;
+
+  if (typeof originalAlt === "string") {
+    const match = originalAlt.match(/(.*) (\[(\d+)%\])?$/);
+    if (match != null) {
+      alt = match[1];
+      dividedBy = match[3] ? parseInt(match[3]) : 100;
+    }
   } else {
-    if (width === null || height === null) {
-      let imageBuffer: Buffer | null = null;
+    alt = originalAlt ?? null;
+  }
 
-      if (src.startsWith("http")) {
-        imageBuffer = Buffer.from(
-          new Uint8Array(
-            (await fetch(src).then(res => res.arrayBuffer())) as ArrayBuffer
-          )
-        );
-      } else {
-        if (
-          !process.env.CI &&
-          process.env.VERCEL_URL &&
-          process.env.NODE_ENV === "production"
-        ) {
-          imageBuffer = Buffer.from(
-            new Uint8Array(
-              (await fetch(
-                "https://" + process.env.VERCEL_URL + src
-              ).then(res => res.arrayBuffer())) as ArrayBuffer
-            )
-          );
-        } else {
-          imageBuffer = await readFile(
-            new URL(
-              join(import.meta.url, "..", "..", "..", "..", "public", src)
-            ).pathname
-          );
-        }
-      }
-      const computedSize = sizeOf(imageBuffer);
-      if (
-        computedSize.width === undefined ||
-        computedSize.height === undefined
-      ) {
-        throw new Error("Could not compute image size");
-      }
-      width = computedSize.width;
-      height = computedSize.height;
-    }
+  const factor = dividedBy / 100;
 
-    let alt: string | null = null;
-    let dividedBy = 100;
-
-    if ("string" === typeof originalAlt) {
-      const match = originalAlt.match(/(.*) (\[(\d+)%\])?$/);
-      if (match != null) {
-        alt = match[1];
-        dividedBy = match[3] ? parseInt(match[3]) : 100;
-      }
-    } else {
-      alt = originalAlt ?? null;
-    }
-
-    const factor = dividedBy / 100;
-
+  if (isRemoteImage) {
+    /* eslint-disable @next/next/no-img-element */
     return (
       <span className="my-5 flex flex-col items-center">
-        <NextImage
-        className="rounded-lg"
-          width={width * factor}
-          height={height * factor}
-          alt={alt ?? ""}
-          src={src}
-        />
-
+        <img className="rounded-lg" src={src} alt={alt ?? ""} />
         {alt && <Caption>{alt}</Caption>}
       </span>
     );
   }
+
+  if (width === null || height === null) {
+    let imageBuffer: Buffer;
+
+    if (
+      !process.env.CI &&
+      process.env.VERCEL_URL &&
+      process.env.NODE_ENV === "production"
+    ) {
+      imageBuffer = Buffer.from(
+        new Uint8Array(
+          (await fetch(
+            "https://" + process.env.VERCEL_URL + src
+          ).then(res => res.arrayBuffer())) as ArrayBuffer
+        )
+      );
+    } else {
+      imageBuffer = await readFile(
+        new URL(
+          join(import.meta.url, "..", "..", "..", "..", "public", src)
+        ).pathname
+      );
+    }
+
+    const computedSize = sizeOf(imageBuffer);
+    if (
+      computedSize.width === undefined ||
+      computedSize.height === undefined
+    ) {
+      throw new Error("Could not compute image size");
+    }
+    width = computedSize.width;
+    height = computedSize.height;
+  }
+
+  return (
+    <span className="my-5 flex flex-col items-center">
+      <NextImage
+        className="rounded-lg"
+        width={width * factor}
+        height={height * factor}
+        alt={alt ?? ""}
+        src={src}
+      />
+
+      {alt && <Caption>{alt}</Caption>}
+    </span>
+  );
 }
